2c41d92188cffc58da1263d43b80bc3e122df584
Tea <Tea@atguigu.com>
2023-03-08 19:57:33 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -18,6 +18,8 @@ public class GitTest {
         System.out.println("hello git3!");
         System.out.println("hello git4!");
         System.out.println("master-fix test!");
+        System.out.println("push test!");
+
 
 
 
