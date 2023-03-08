a3afdb933b20f2532792a722841f6dd8f38c3932
Tea <Tea@atguigu.com>
2023-03-08 18:15:27 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * ClassName: GitTest
+ * Package: com.atguigu.git
+ * Description
+ *
+ * @Author: Tea
+ * @Create: 2023/3/8 - 18:11
+ * @Version: v1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
