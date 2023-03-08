d09b1c091adc8fba2174aec650260d4f9b6be769
Tea <Tea@atguigu.com>
2023-03-08 18:17:54 +0800

    second commit

---
+++ src/main/java/com/atguigu/git/Main.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * ClassName: ${NAME}
+ * Package: com.atguigu
+ * Description
+ *
+ * @Author: Tea
+ * @Create: ${DATE} - ${TIME}
+ * @Version: v1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
